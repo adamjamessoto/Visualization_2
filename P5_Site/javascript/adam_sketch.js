5254e8273f20cb7db49e7b27c84ea599d4bdcba8
adamjamessoto <adamjamessoto@utexas.edu>
2016-10-24 20:28:19 -0500

    Added hardcoding since Yahoo API JSON is down

--- P5_Site/javascript/adam_sketch.js
+++ P5_Site/javascript/adam_sketch.js
@@ -1,41 +1,45 @@
 var stocks;
 function preload() {
-  var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20symbol%20,%20Ask%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22NKE%22,%20%22GM%22,%20%22AAPL%22,%20%22MSFT%22)&env=store://datatables.org/alltableswithkeys&format=json';
-  stocks = loadJSON(url);
+  //var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20symbol%20,%20Ask%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22NKE%22,%20%22GM%22,%20%22AAPL%22,%20%22MSFT%22)&env=store://datatables.org/alltableswithkeys&format=json';
+  //stocks = loadJSON(url);
 }
-
 
 function setup() {
   var canvas = createCanvas(windowWidth*0.98, windowHeight*0.97);
   background(255);
   stroke(0);
   strokeWeight(5);
-  console.log(stocks.query.results.quote[0].symbol);
-  // an array with some stockPrices
-  var stockPrices = [116.54, 800.00, 59.65];
-  var stockNames = ["Apple Inc.", "Alphabet Inc.", "Microsoft Corporation"];
 
+  // ********** an array with some stockPrices **********
+  var stockPrices = [51.87, 32.98, 117.65, 61.00];
+  var stockNames = ["NKE", "GM", "AAPL", "MSFT"];
 
-  // calculate a few sizes
+  // ********** calculate a few sizes **********
   var width = (windowWidth * 0.7 * 0.98) / stockPrices.length;
   var spacer = (windowWidth * 0.3 * 0.98) / (stockPrices.length + 1);
+  //var width = (windowWidth * 0.7 * 0.98) / stocks.query.results.quote.length;
+  //var spacer = (windowWidth * 0.3 * 0.98) / (stocks.query.results.quote.length + 1);
   var halfHeight = (windowHeight*0.97) / 2;
   var scale = 100 / Math.max.apply(null, stockPrices);
+  //var scale = 100 / Math.max.apply(null, stocks.query.results.quote);
 
   console.log(scale);
 
-  // create an instance of scribble and set a few parameters
+  // ********** create an instance of scribble and set a few parameters **********
   var scribble = new Scribble();
   scribble.bowing = 0.1;
   scribble.roughness = 1.5;
 
-  // draw a horizontal line across the center of the screen
+  // ********** draw a horizontal line across the center of the screen **********
   scribble.scribbleLine(0, halfHeight, windowWidth, halfHeight);
 
-  // draw every value as a filled rect in a bar graph
+  // ********** draw every value as a filled rect in a bar graph **********
   for (var i = 0; i < stockPrices.length; i++) {
-    // calculate the x and y coordinates of the center of the rect and the height
-    var h = halfHeight * 0.01 * stocks.query.results.quote[0].Ask[i] * scale;
+  //for (var i = 0; i < stocks.query.results.quote.length; i++) {
+
+    // ********** calculate the x and y coordinates of the center of the rect and the height **********
+    var h = halfHeight * 0.01 * stockPrices[i] * scale;
+    //var h = halfHeight * 0.01 * stocks.query.results.quote[0].Ask[i] * scale;
     var x = (spacer + width) * ( i + 1 ) - (width / 2);
     var y = halfHeight - h / 2;
 
@@ -48,11 +52,13 @@ function setup() {
     // draw a rect for the value
     scribble.scribbleRect( x, y, width, h );
 
-    // calculate the x and y coordinates for the border points of the hachure
+    // ********** calculate the x and y coordinates for the border points of the hachure **********
     var xleft = x - width / 2 + 5;
     var xright = x + width / 2 - 5;
-    var ytop = y - (halfHeight *  0.01 * stocks.query.results.quote[0].Ask[i] * scale / 2);
-    var ybottom = y + (halfHeight *  0.01 * stocks.query.results.quote[0].Ask[i] * scale / 2);
+    var ytop = y - (halfHeight *  0.01 * stockPrices[i] * scale / 2);
+    var ybottom = y + (halfHeight *  0.01 * stockPrices[i] * scale / 2);
+    //var ytop = y - (halfHeight *  0.01 * stocks.query.results.quote[0].Ask[i] * scale / 2);
+    //var ybottom = y + (halfHeight *  0.01 * stocks.query.results.quote[0].Ask[i] * scale / 2);
 
     // reduce the sizes to fit in the rect
     if (ytop > ybottom) {
@@ -87,7 +93,8 @@ function setup() {
     
     strokeWeight(1);
     stroke(2);
-    text(stocks.query.results.quote[i].symbol, x - 30, ybottom + 30);
+    text(stockNames[i], x - 20, ybottom + 30);
+    //text(stocks.query.results.quote[i].symbol, x - 30, ybottom + 30);
   }
 }
 
